dd5ced47022678f5385bc2ba0a0b6426c0b275a7
ernest <efran7107@gmail.com>
2024-03-22 20:02:07 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Section } from "./Components/Section";
+import { CreateDogForm } from './Components/CreateDogForm';
+import { Dogs } from './Components/Dogs';
+import { Section } from './Components/Section';
 
 export function App() {
-  return (
-    <div className="App" style={{ backgroundColor: "skyblue" }}>
-      <header>
-        <h1>pup-e-picker (Functional)</h1>
-      </header>
-      <Section label={"Dogs: "}>
-
-      </Section>
-    </div>
-  );
+	return (
+		<div
+			className='App'
+			style={{ backgroundColor: 'skyblue' }}>
+			<header>
+				<h1>pup-e-picker (Functional)</h1>
+			</header>
+			<Section label={'Dogs: '}>
+				<Dogs />
+				<CreateDogForm />
+			</Section>
+		</div>
+	);
 }
